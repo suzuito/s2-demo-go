3a5e451d9f12b76fe724505ffd794958caa379de
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-04-17 18:48:48 +0900

    save

--- page/pagepoint01/main.go
+++ page/pagepoint01/main.go
@@ -15,6 +15,12 @@ func main() {
 	fmt.Printf("緯度=%f 経度=%f（度数表現）\n", l.Lat.Degrees(), l.Lng.Degrees())
 	fmt.Printf("IsValid=%v\n", l.IsValid())
 
+	// 注意
+	// String関数は度数表現を出力する
+	// （実際には、Lat Lng はラジアン表現の値が格納されている）
+	fmt.Printf("String %s\n", l.String())
+	fmt.Printf("String %s\n", l)
+
 	// ダメな例
 	l = s2.LatLng{Lat: 35.6938, Lng: 139.7034} // 新宿駅の座標を誤って設定
 	fmt.Printf("緯度=%f 経度=%f（ラジアン表現）誤\n", l.Lat, l.Lng)
