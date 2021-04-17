2179bd213c71f5c67be1cec2d736e2bd25f4c7eb
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-04-17 18:15:52 +0900

    save

--- page/pagepoint01/main.go
+++ page/pagepoint01/main.go
@@ -7,7 +7,39 @@ import (
 )
 
 func main() {
-	latlng := s2.LatLngFromDegrees(35.6938, 139.7034)
-	fmt.Printf("緯度=%f 経度=%f （ラジアン表現）\n", latlng.Lat, latlng.Lng)
-	fmt.Printf("緯度=%f 経度=%f （度数表現）\n", latlng.Lat.Degrees(), latlng.Lng.Degrees())
+	var latlng s2.LatLng
+
+	// 良い例
+	latlng = s2.LatLngFromDegrees(35.6938, 139.7034) // 新宿駅の座標
+	fmt.Printf(
+		"緯度=%f 経度=%f（ラジアン表現）\n",
+		latlng.Lat,
+		latlng.Lng,
+	)
+	fmt.Printf(
+		"緯度=%f 経度=%f（度数表現）\n",
+		latlng.Lat.Degrees(),
+		latlng.Lng.Degrees(),
+	)
+	fmt.Printf(
+		"IsValid=%v\n",
+		latlng.IsValid(),
+	)
+
+	// ダメな例
+	latlng = s2.LatLng{Lat: 35.6938, Lng: 139.7034} // 新宿駅の座標を誤って設定
+	fmt.Printf(
+		"緯度=%f 経度=%f（ラジアン表現）誤\n",
+		latlng.Lat,
+		latlng.Lng,
+	)
+	fmt.Printf(
+		"緯度=%f 経度=%f（度数表現）誤\n",
+		latlng.Lat.Degrees(),
+		latlng.Lng.Degrees(),
+	)
+	fmt.Printf(
+		"IsValid=%v\n",
+		latlng.IsValid(),
+	)
 }
