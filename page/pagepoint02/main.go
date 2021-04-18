b65085117921330924aa39e1b17ae932fb6395f2
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-04-18 10:33:38 +0900

    save

--- page/pagepoint02/main.go
+++ page/pagepoint02/main.go
@@ -9,9 +9,13 @@ import (
 func main() {
 	l1 := s2.LatLngFromDegrees(35.6938, 139.7034) // 新宿駅の座標
 
-	p := s2.PointFromLatLng(l1)
+	p1 := s2.PointFromLatLng(l1)
 	fmt.Printf("緯度=%f 経度=%f（ラジアン表現）\n", l1.Lat, l1.Lng)
 	fmt.Printf("緯度=%f 経度=%f（度数表現）\n", l1.Lat.Degrees(), l1.Lng.Degrees())
-	fmt.Printf("Point x=%f y=%f z=%f\n", p.X, p.Y, p.Z)
-	fmt.Printf("Point Norm=%f\n", p.Norm())
+	fmt.Printf("Point x=%f y=%f z=%f\n", p1.X, p1.Y, p1.Z)
+	fmt.Printf("Point 3 次元ベクトルの長さ=%f\n", p1.Norm())
+
+	p2 := s2.PointFromCoords(10000, 0, 0) // 入力されるベクトルは長さが 10000
+	fmt.Printf("Point x=%f y=%f z=%f\n", p2.X, p2.Y, p2.Z)
+	fmt.Printf("Point 3 次元ベクトルの長さ=%f\n", p2.Norm())
 }
