991fcc87e152f7fa40799aa10830781b02dba150
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-04-18 20:54:32 +0900

    save

--- page/pageccw01/main.go
+++ page/pageccw01/main.go
@@ -7,23 +7,13 @@ import (
 	"github.com/suzuito/s2-demo-go/s2geojson"
 )
 
-func dirToName(d s2.Direction) string {
-	if d == s2.Clockwise {
-		return "時計回り"
-	}
-	if d == s2.CounterClockwise {
-		return "反時計回り"
-	}
-	return "不定"
-}
-
 func main() {
 	a := s2.PointFromLatLng(s2.LatLngFromDegrees(43.0618, 141.3545)) // 札幌
 	b := s2.PointFromLatLng(s2.LatLngFromDegrees(35.6804, 139.7690)) // 東京
 	c := s2.PointFromLatLng(s2.LatLngFromDegrees(33.5902, 130.4017)) // 福岡
 
-	fmt.Printf("東京->札幌->福岡 %s\n", dirToName(s2.RobustSign(b, a, c)))
-	fmt.Printf("東京->福岡->札幌 %s\n", dirToName(s2.RobustSign(b, c, a)))
+	fmt.Printf("東京->札幌->福岡 %d\n", s2.RobustSign(b, a, c))
+	fmt.Printf("東京->福岡->札幌 %d\n", s2.RobustSign(b, c, a))
 
 	// ウラジオストク
 	o := s2.PointFromLatLng(s2.LatLngFromDegrees(43.1332, 131.9113))
