5bfc1cffc63a1eba2381b9ce65587c472c9db544
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-05-10 23:08:32 +0900

    Save

--- data/article/05_ccw01/01/main.go
+++ data/article/05_ccw01/01/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/golang/geo/r3"
 	"github.com/golang/geo/s2"
 	"github.com/suzuito/s2-demo-go/s2geojson"
 )
@@ -15,14 +16,13 @@ func main() {
 	fmt.Printf("東京->札幌->福岡 %d\n", s2.RobustSign(b, a, c))
 	fmt.Printf("東京->福岡->札幌 %d\n", s2.RobustSign(b, c, a))
 
-	// ウラジオストク
-	o := s2.PointFromLatLng(s2.LatLngFromDegrees(43.1332, 131.9113))
+	o := s2.Point{Vector: r3.Vector{X: 0, Y: 0, Z: 0}}
 	fmt.Printf(
-		"東京->札幌->福岡 は、ウラジオストクから見たとき、反時計回りですか？ %v\n",
+		"東京->札幌->福岡 は、原点(0,0,0)から見たとき、反時計回りですか？ %v\n",
 		s2.OrderedCCW(b, a, c, o))
 	fmt.Printf(
-		"ウラジオストク->札幌->福岡 は、東京から見たとき、反時計回りですか？ %v\n",
-		s2.OrderedCCW(o, a, c, b))
+		"東京->福岡->札幌 は、原点(0,0,0)から見たとき、反時計回りですか？ %v\n",
+		s2.OrderedCCW(b, c, a, o))
 
 	s2geojson.Print(&[]s2.Point{b, c, a, o}, &s2geojson.PrintGeoJSONOption{
 		StyleHeight: "500px",
